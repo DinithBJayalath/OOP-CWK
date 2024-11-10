b51e6cc21d9f6cdb2cf4ac4e6a035673041c11d8
DinithBJayalath <dinithbjayalath@gmail.com>
2024-11-10 11:08:51 +0530

    Initialized the backend

---
+++ OOP-CWK-Backend/src/main/java/com/example/oopcwkbackend/OopCwkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopcwkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopCwkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopCwkBackendApplication.class, args);
+	}
+
+}
---
+++ OOP-CWK-Backend/src/test/java/com/example/oopcwkbackend/OopCwkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopcwkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopCwkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
